799bf12d2137622095e64d910471ca9c8dd40f55
David Ramirez <100328945+DavidR9830@users.noreply.github.com>
2024-02-20 09:52:00 -0500

    Se añadio botón de crear alarma y botón de tomar foto También esta añadido el botón de enviar correo, pero falta termianr la funcionalidad

--- app/src/main/java/com/david/appandroid/SecondActivity.java
+++ app/src/main/java/com/david/appandroid/SecondActivity.java
@@ -1,6 +1,14 @@
 package com.david.appandroid;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.AlarmClock;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +17,21 @@ public class SecondActivity extends AppCompatActivity {
 
     private TextView displayTextView;
 
+    private Button alarmButton;
+
+    private Button cameraButton;
+
+    private Button correoButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
         displayTextView = findViewById(R.id.displayTextView);
+        alarmButton = findViewById(R.id.alarmButton);
+        cameraButton = findViewById(R.id.cameraButton);
+        correoButton = findViewById(R.id.correoButton);
 
         // Obtener los datos del intent
         String username = getIntent().getStringExtra("username");
@@ -22,5 +39,57 @@ public class SecondActivity extends AppCompatActivity {
 
         // Mostrar los datos en pantalla
         displayTextView.setText("Bienvenido, " + name  +"("+username+")");
+
+
+        alarmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createAlarm("Champions moment", 15,0);
+            }
+        });
+
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
+
+
     }
+
+
+    public void createAlarm(String message, int hour, int minutes) {
+        Intent intent = new Intent(AlarmClock.ACTION_SET_ALARM)
+                .putExtra(AlarmClock.EXTRA_MESSAGE, message)
+                .putExtra(AlarmClock.EXTRA_HOUR, hour)
+                .putExtra(AlarmClock.EXTRA_MINUTES, minutes);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        try {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        } catch (ActivityNotFoundException e) {
+            // Display error state to the user.
+        }
+    }
+
+    public void composeEmail(String[] addresses, String subject, Uri attachment) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("*/*");
+        intent.putExtra(Intent.EXTRA_EMAIL, addresses);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_STREAM, attachment);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
 }
